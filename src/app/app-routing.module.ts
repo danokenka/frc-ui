d3920c3fe2e4a8cb902b37fa2a98298e253cf8ae
Waterbrother Solutions <waterbrosol@Waterbrothers-MacBook-Pro.local>
2022-06-03 18:10:44 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,28 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/frc/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'test',
+    loadChildren: () => import('./pages/frc/final/test/test.module').then( m => m.TestPageModule)
+  },
+  {
+    path: 'test',
+    loadChildren: () => import('./pages/frc/dev/test/test.module').then( m => m.TestPageModule)
+  },
+  {
+    path: 'accordian',
+    loadChildren: () => import('./pages/frc/examples/accordian/accordian.module').then( m => m.AccordianPageModule)
+  },
+  {
+    path: 'avatar',
+    loadChildren: () => import('./pages/frc/examples/avatar/avatar.module').then( m => m.AvatarPageModule)
   },
 ];
 
